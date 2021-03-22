f5945ab37ec4693b69d67d25dd11785289e27703
alejovasquero <alejo_704@hotmail.com>
2021-03-22 17:03:11 -0500

    :wrench: Sonar test

--- src/test/java/com/eci/ieti/Sonar.java
+++ src/test/java/com/eci/ieti/Sonar.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.junit.Test;
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SonarTest {
+public class Sonar {
 
     @Test
     public void contextLoads() {
