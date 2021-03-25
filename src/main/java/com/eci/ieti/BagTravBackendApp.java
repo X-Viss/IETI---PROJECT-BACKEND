0eb778e15b5cc9abb46951feb085a5f4acd34179
Wasawsky <mjbc_20@hotmail.com>
2021-03-24 23:27:00 -0500

    skeleton

---
+++ src/main/java/com/eci/ieti/BagTravBackendApp.java
@@ -0,0 +1,12 @@
+package com.eci.ieti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BagTravBackendApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BagTravBackendApp.class, args);
+	}
+}
---
+++ src/main/java/com/eci/ieti/services/impl/BagTravServiceImpl.java
@@ -0,0 +1,7 @@
+package com.eci.ieti.services.impl;
+
+import com.eci.ieti.services.BagTravService;
+
+public class BagTravServiceImpl implements BagTravService{
+    
+}
