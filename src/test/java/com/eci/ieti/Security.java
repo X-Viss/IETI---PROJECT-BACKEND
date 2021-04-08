a6df78ff741f566a88e06ae4ea2fff2b9e9ff004
Lfersar <luisafsar@gmail.com>
2021-04-08 16:28:55 -0500

    :white_check_mark: test

--- src/test/java/com/eci/ieti/Security.java
+++ src/test/java/com/eci/ieti/Security.java
@@ -1,6 +1,7 @@
 package com.eci.ieti;
 
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -8,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultMatcher;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -27,15 +29,26 @@ public class Security {
     
     @Test
     public void Autenticated()throws Exception{
-        System.out.println("-----------------------------------------------------");
-        MvcResult mvcResult = mockMvc.perform(post("/subs")).andDo(print())
-        .andExpect(status().isOk()).andExpect(content()
-        .contentType("application/json;charset=UTF-8"))
-        .andExpect(jsonPath("$.userName").value("Luisa"))
-        .andExpect(jsonPath("$.password").value("psw"))
+        MvcResult mvcResult = mockMvc.perform(post("/subs").contentType("application/json")
+        .content("{\"userName\" : \"Luisa\",\"password\" : \"psw\"}"))
+        .andDo(print())
+        .andExpect(status().isOk())
         .andReturn();
-        System.out.println("-----------------------------------------------------");
-        System.out.println(mvcResult.toString());
-    
+        Assertions.assertEquals("{\"response\":\"Succesful subscription for client Luisa\"}",mvcResult.getResponse().getContentAsString());
     }
+
+    public void authenticateClient() throws Exception{
+        mockMvc.perform(post("/subs").contentType("application/json")
+        .content("{\"userName\" : \"Luisa\",\"password\" : \"psw\"}"))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andReturn();
+        MvcResult mvcResult2 = mockMvc.perform(post("/subs").contentType("application/json")
+        .content("{\"userName\" : \"Luisa\",\"password\" : \"psw\"}"))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andReturn();
+        Assertions.assertEquals("{\"response\":\"Succesful Authentication for client Luisa\"}",mvcResult2.getResponse().getContentAsString());
+    }
+
 }
