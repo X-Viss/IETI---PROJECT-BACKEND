8104bdf17f3a5816a6fccafbe8ad9cddefdeb4f2
sebastian nieto <juan.nieto-mo@mail.escuelaing.edu.co>
2021-04-08 10:35:59 -0500

    correccion

--- src/main/java/com/eci/ieti/SaveData.java
+++ src/main/java/com/eci/ieti/SaveData.java
@@ -10,7 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.ArrayList;
 import java.util.List;
 
-@SpringBootApplication
 public class SaveData implements CommandLineRunner {
 
     @Autowired
