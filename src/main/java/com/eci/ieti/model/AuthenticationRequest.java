abf6a3aee4e065e650d9fa5ce4341404895ca6a6
Lfersar <luisafsar@gmail.com>
2021-04-10 01:24:00 -0500

    :recycle: User

--- src/main/java/com/eci/ieti/model/AuthenticationRequest.java
+++ src/main/java/com/eci/ieti/model/AuthenticationRequest.java
@@ -1,8 +1,13 @@
 package com.eci.ieti.model;
 
+import java.util.Date;
+
 public class AuthenticationRequest {
     private String userName;
     private String password;
+    private Integer phone;
+    private Date birth;
+    private String country;
 
     public AuthenticationRequest(){
         // Constructor .
@@ -22,6 +27,30 @@ public class AuthenticationRequest {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
     }
 
 }
