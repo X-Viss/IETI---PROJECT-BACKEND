9f04073944cd63c2bb8bc1e92a4e5a45b9e0e013
Wasawsky <mjbc_20@hotmail.com>
2021-04-15 17:17:50 -0500

    fix

--- src/main/java/com/eci/ieti/controller/TravelController.java
+++ src/main/java/com/eci/ieti/controller/TravelController.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:3000")
